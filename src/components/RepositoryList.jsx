2df1d59e48c53bc2fa2a5b2fd349e30464f0fa0c
Ridwan Ijadunola <dareedyone@gmail.com>
2020-10-10 01:04:40 +0100

    Exercise 10.24: filtering the reviewed repositories list

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { FlatList, View, StyleSheet } from "react-native";
 import RepositoryItem from "./RepositoryItem";
 import useRepositories from "../hooks/useRepositories";
 import { useHistory } from "react-router-native";
 import RNPickerSelect from "react-native-picker-select";
+import { Searchbar } from "react-native-paper";
+import { useDebounce } from "use-debounce";
 
 const styles = StyleSheet.create({
 	container: {
@@ -61,74 +63,123 @@ const styles = StyleSheet.create({
 // 		ownerAvatarUrl: "https://avatars3.githubusercontent.com/u/13142323?v=4",
 // 	},
 // ];
-let selectedValue;
+const SearchBarComp = ({ setReposOption }) => {
+	const [searchText, setSearchText] = React.useState("");
+	const [searchKeyword] = useDebounce(searchText, 500);
+	const onChangeSearch = (query) => setSearchText(query);
+	useEffect(() => {
+		setReposOption({ searchKeyword });
+	}, [searchKeyword]);
+
+	return (
+		<Searchbar
+			style={{ marginBottom: 15 }}
+			placeholder="Search"
+			onChangeText={onChangeSearch}
+			value={searchText}
+		/>
+	);
+};
+//since class component is used i don''t have to set a global var inacse component rerender to reset value
 export const Dropdown = ({ setReposOption }) => {
 	return (
-		<View style={{ paddingHorizontal: 15 }}>
-			<RNPickerSelect
-				value={selectedValue}
-				onValueChange={(value) => {
-					setReposOption(JSON.parse(value));
-					selectedValue = value;
-				}}
-				items={[
-					{
-						label: "Latest Repositories",
-						value: JSON.stringify({
-							orderBy: "CREATED_AT",
-							orderDirection: "DESC",
-						}),
-					},
-					{
-						label: "Highest rated Repositories",
-						value: JSON.stringify({
-							orderBy: "RATING_AVERAGE",
-							orderDirection: "DESC",
-						}),
-					},
-					{
-						label: "Lowest rated Repositories",
-						value: JSON.stringify({
-							orderBy: "RATING_AVERAGE",
-							orderDirection: "ASC",
-						}),
-					},
-				]}
-			/>
-		</View>
+		<RNPickerSelect
+			onValueChange={(value) => {
+				setReposOption(JSON.parse(value));
+			}}
+			items={[
+				{
+					label: "Latest repositories",
+					value: JSON.stringify({
+						orderBy: "CREATED_AT",
+						orderDirection: "DESC",
+					}),
+				},
+				{
+					label: "Highest rated repositories",
+					value: JSON.stringify({
+						orderBy: "RATING_AVERAGE",
+						orderDirection: "DESC",
+					}),
+				},
+				{
+					label: "Lowest rated repositories",
+					value: JSON.stringify({
+						orderBy: "RATING_AVERAGE",
+						orderDirection: "ASC",
+					}),
+				},
+			]}
+		/>
 	);
 };
 const ItemSeparator = () => <View style={styles.separator} />;
 
-export const RepositoryListContainer = ({ repositories, setReposOption }) => {
-	// Get the nodes from the edges array
-	const history = useHistory();
-	// console.log("setRepo REACH RPLC", setReposOption);
-	// console.log(repositories);
-	return (
-		<FlatList
-			data={repositories}
-			ItemSeparatorComponent={ItemSeparator}
-			ListHeaderComponent={() => <Dropdown setReposOption={setReposOption} />}
-			renderItem={({ item }) => (
-				<RepositoryItem item={item} history={history} />
-			)}
-			keyExtractor={(item) => item.id}
+// export const RepositoryListContainer = ({ repositories, setReposOption }) => {
+// 	// Get the nodes from the edges array
+// 	const history = useHistory();
+// 	// console.log("setRepo REACH RPLC", setReposOption);
+// 	// console.log(repositories);
+// 	return (
+// 		<FlatList
+// 			data={repositories}
+// 			ItemSeparatorComponent={ItemSeparator}
+// 			ListHeaderComponent={() => (
+// 				<View style={{ padding: 15 }}>
+// 					<SearchBarComp setReposOption={setReposOption} />
+// 					<Dropdown setReposOption={setReposOption} />
+// 				</View>
+// 			)}
+// 			renderItem={({ item }) => (
+// 				<RepositoryItem item={item} history={history} />
+// 			)}
+// 			keyExtractor={(item) => item.id}
 
-			// other props
-		/>
-	);
-};
+// 			// other props
+// 		/>
+// 	);
+// };
+
+export class RepositoryListContainer extends React.Component {
+	renderHeader = () => {
+		// this.props contains the component's props
+		const props = this.props;
+		return (
+			<View style={{ padding: 15 }}>
+				<SearchBarComp setReposOption={props.setReposOption} />
+				<Dropdown setReposOption={props.setReposOption} />
+			</View>
+		);
+	};
+
+	render() {
+		return (
+			<FlatList
+				data={this.props.repositories}
+				ItemSeparatorComponent={ItemSeparator}
+				ListHeaderComponent={this.renderHeader}
+				renderItem={({ item }) => (
+					<RepositoryItem item={item} history={this.props.history} />
+				)}
+				keyExtractor={(item) => item.id}
+
+				// other props
+			/>
+		);
+	}
+}
 
 const RepositoryList = () => {
 	const { repositories, setReposOption } = useRepositories();
 	const repositoryNodes = repositories
 		? repositories?.edges?.map((edge) => edge.node)
 		: [];
+	const history = useHistory();
 	return (
 		<RepositoryListContainer
 			setReposOption={setReposOption}
 			repositories={repositoryNodes}
+			history={history}
 		/>
 	);
 };
